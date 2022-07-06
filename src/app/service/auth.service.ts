866bb769a05af18478e2c1c94b6a070e6353fce0
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-07-06 14:53:17 -0300

    implementando a página Início

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -19,5 +20,15 @@ export class AuthService {
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
     return this.http.post<UsuarioLogin>('https://bloghistoriarte.herokuapp.com/usuarios/logar', usuarioLogin)
+  }
+
+  logado(){
+
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,3 +5,4 @@ export const environment = {
   id: 0,
   foto: '',
 };
+ 
