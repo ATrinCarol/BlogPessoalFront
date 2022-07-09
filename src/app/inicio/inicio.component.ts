9d6440183d33034ac4a0f4f2323f7d2c5971ab21
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-07-09 03:38:06 -0300

    implementação do método post

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { subscribeOn } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,16 +15,61 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+
+  tema: Tema = new Tema()
+  listTema: Tema[]
+  idTema: number
+
+  usuario: Usuario = new Usuario()
+  idUsuario = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
+
   ) { }
 
   ngOnInit() {
 
-    if(environment.token == ''){
-     // alert ('Sua sessão expirou. Faça o login novamente!')
+    if (environment.token == '') {
+      // alert ('Sua sessão expirou. Faça o login novamente!')
       this.router.navigate(['/entrar'])
     }
-  } 
+
+    this.findAllTema()
+  }
+
+  findAllTema() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listTema = resp
+    })
+  }
+
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+
+  }
+
+  publicar() {
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert('Sua postagem foi realizada com sucesso')
+      this.postagem = new Postagem()
+    })
+  }
+
+
 
 }
+
+
