bda8ff3c1aa42364363e22177db47ab109e37eaf
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-07-11 19:48:35 -0300

    implementação put de usuário

---
+++ src/app/edit/user-edit/user-edit.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/model/Usuario';
+import { AuthService } from 'src/app/service/auth.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit {
+
+  usuario: Usuario = new Usuario()
+
+  idUsuario: number
+
+  confirmSenha: string
+  tipoUsuaria: string
+
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router,
+
+  ) { }
+
+  ngOnInit() {
+
+    window.scroll(0, 0)
+
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idUsuario = this.route.snapshot.params['id']
+    this.findByIdUser(this.idUsuario)
+  }
+
+  confirmarSenha(event: any) {
+    this.confirmSenha = event.target.value
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoUsuaria = event.target.value
+  }
+
+  atualizar() {
+    this.usuario.tipo = this.tipoUsuaria
+
+    if (this.usuario.senha != this.confirmSenha) {
+      alert('As senhas não coincidem. Digite corretamente!')
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp
+        this.router.navigate(['/inicio'])
+        alert('Atualização realizada com sucesso! Faça o Login novamente!')
+        environment.token = ''
+        environment.nome = ''
+        environment.foto = ''
+        environment.id = 0
+        this.router.navigate(['/entrar'])
+      })
+    }
+  }
+  //TENTATIVA DO USO PUT
+  // atualizar() {
+  //   this.usuario.id = this.idUsuario
+  //   this.usuario.tipo = this.tipoUsuaria
+
+  //   if (this.usuario.senha != this.confirmSenha) {
+  //     alert('As senhas não coincidem. Digite corretamente!')
+  //   } else {
+  //     this.authService.recadastrar(this.usuario).subscribe((resp: Usuario) => {
+  //       this.usuario = resp
+  //       alert('Atualização realizada com sucesso!')
+  //       this.router.navigate(['/inicio'])
+  //     })
+  //   }
+  // }
+
+  findByIdUser(id: number) {
+    this.authService.getByIdUsuario(id).subscribe((resp: Usuario) => {
+      this.usuario = resp
+    })
+  }
+
+}
